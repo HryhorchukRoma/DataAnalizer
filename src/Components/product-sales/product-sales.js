707e29615741ea1911f65650049fcbd925676a51
HryhorchukRoma <124773242+HryhorchukRoma@users.noreply.github.com>
2025-02-27 12:45:04 +0200

    Update product-sales.js

--- src/Components/product-sales/product-sales.js
+++ src/Components/product-sales/product-sales.js
@@ -15,7 +15,7 @@ const columns = [
     dataIndex: 'price', 
     key: 'price',
     sorter: (a, b) => parseFloat(a.price) - parseFloat(b.price),
-    render: (text) => `{text}`,
+    render: (text) => `${text}`,
   },
 ];
 
